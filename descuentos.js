93565edf56b51f82e97db402ba39cc90f6371139
David <davidrodrilopez2002@gmail.com>
2022-06-22 15:55:04 -0400

    culminaacion de proyecto de decuentos

--- descuentos.js
+++ descuentos.js
@@ -1,17 +1,34 @@
-// const precioOriginal = 250;
-// const descuento = 30;
-
 function calcularPrecioConDescuento(precio, descuento){
     const porcentajePrecioConDescuento = 100 - descuento;
     const precioConDescuento = (precio * porcentajePrecioConDescuento)/100;
     return precioConDescuento;
 }
+let coupon = ["Dabi_gei", "LloreLok", "geigeigeigei123"];
+
+function onClickButtonPriceDiscount(){
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+    const inputCoupon = document.getElementById("InputCoupon");
+    const couponValue = inputCoupon.value
+    let discount;
+    switch (couponValue) {
+        case coupon[0]:
+            discount = 15;
+            break;
+        case coupon[1]:
+            discount = 30;
+            break;
+        case coupon[2]:
+            discount = 50;
+            break;
+        default:
+            discount = 0
+            break;
+    }
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discount);
+    const resultPrice = document.getElementById("ResultPrice");
+    resultPrice.innerText = `El precio total con el descuento es de ${precioConDescuento} COP`
+}
 
 
 
-//console.log({
-//    precioOriginal,
-//    descuento,
-//    porcentajePrecioConDescuento,
-//    precioConDescuento,
-//});
